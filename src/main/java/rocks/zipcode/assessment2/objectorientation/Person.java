871728edec668d7ebf245a9c6aa6cc92d3fe3a82
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-04 11:49:17 -0500

    Object orientation

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -5,40 +5,72 @@ package rocks.zipcode.assessment2.objectorientation;
  * @ATTENTION_TO_STUDENTS - Ensure that you have completed the `Address` class before attempting this class
  */
 public class Person {
+    private Long id;
+    private String name;
+    private Address address;
+
     /**
      * @param id - id of person
      * @param name - name of person
      * @param address - address of person
      */
     public Person(Long id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     }
 
     public Person() {
+        id = Long.MIN_VALUE;
+        name = "";
+        address = new Address();
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        if(o == null || !(o instanceof Person))
+            return false;
+        Person p = (Person)o;
+        boolean isEqual = true;
+        if(this.name!= null)
+            isEqual = p.getName().equals(this.name);
+
+        if(this.id!= null)
+            isEqual = p.getId().equals(id);
+
+        return isEqual;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
     }
 }
